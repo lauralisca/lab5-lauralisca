d79bcc95fd15273591d80abf0cb473352234d7e1
Laura Lisca <lauralisgo@unisabana.edu.co>
2026-03-17 22:13:38 -0500

    lab 5

--- caller.py
+++ caller.py
@@ -1,9 +1,10 @@
-
+from mystery_module import transform_data
 
 # FREEZE CODE BEGIN
 x = int(input())
 y = float(input())
 # FREEZE CODE END
 
-result = (x+y)/2
-print(float(result))
+result = transform_data(x, y, "quiz_test")
+print(result)
+
